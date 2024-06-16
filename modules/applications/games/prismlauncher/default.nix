1f6baa64dbb283545bf0ffc267ccdb4b5d4e3df4
Po Co <polniolino@gmail.com>
2024-06-16 11:42:47 +0200

    Added Catppuccin theme to PrismLauncher

--- modules/applications/games/prismlauncher/default.nix
+++ modules/applications/games/prismlauncher/default.nix
@@ -1,1 +1,16 @@
-{ pkgs, ... }: { os.environment.systemPackages = [ pkgs.prismlauncher ]; }
+{ pkgs, inputs, ... }: {
+  inputs = {
+    prismlauncher-catppuccin = {
+      url =
+        "https://github.com/PrismLauncher/Themes/releases/latest/download/Catppuccin-Mocha-theme.zip";
+      flake = false;
+    };
+  };
+
+  os.environment.systemPackages = [ pkgs.prismlauncher ];
+
+  hm.xdg.dataFile."PrismLauncher/themes/Catppuccin-Mocha" = {
+    source = "${inputs.prismlauncher-catppuccin}/Catppuccin-Mocha";
+    recursive = true;
+  };
+}
