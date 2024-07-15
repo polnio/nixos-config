e57fdb845349eccf0ce9b214cf57b0ab6ae501aa
Po Co <polniolino@gmail.com>
2024-07-16 01:19:02 +0200

    Fix fcitx5 breacking changes

--- modules/config/keyboard/default.nix
+++ modules/config/keyboard/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   os.i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5 = {
       addons = [ pkgs.fcitx5-mozc pkgs.fcitx5-gtk pkgs.fcitx5-configtool ];
     };
