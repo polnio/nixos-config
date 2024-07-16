f508722b7911ba78a25c34a920f8e252162b9fa0
Po Co <polniolino@gmail.com>
2024-07-16 02:03:12 +0200

    Fix nobar option

--- modules/applications/windows-managers/hyprland/plugins/hyprbars.nix
+++ modules/applications/windows-managers/hyprland/plugins/hyprbars.nix
@@ -1,16 +1,24 @@
-{ pkgs, osConfig, ... }:
+{
+  pkgs,
+  config,
+  osConfig,
+  ...
+}:
 with osConfig.stylix;
 with base16Scheme;
 {
   hm.wayland.windowManager.hyprland = {
     plugins = [ pkgs.hyprlandPlugins.hyprbars ];
-    settings.plugin.hyprbars = {
-      bar_color = "0xff${base00}";
-      bar_height = 45;
-      bar_padding = 12;
-      hyprbars-button = [ "0xff${base02}, 22, 󰖭, hyprctl dispatch killactive" ];
-      bar_part_of_window = true;
-      bar_precedence_over_border = true;
+    settings = {
+      plugin.hyprbars = {
+        bar_color = "0xff${base00}";
+        bar_height = 45;
+        bar_padding = 12;
+        hyprbars-button = [ "0xff${base02}, 22, 󰖭, hyprctl dispatch killactive" ];
+        bar_part_of_window = true;
+        bar_precedence_over_border = true;
+      };
+      windowrule = builtins.map (window: "plugin:hyprbars:nobar,${window}") config.settings.windows.noBar;
     };
   };
 }
