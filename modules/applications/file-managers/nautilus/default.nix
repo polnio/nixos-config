3211df29818ed402081bfb227745c3b1b7df576d
Po Co <polniolino@gmail.com>
2024-07-31 10:47:49 +0200

    Fix audio metadata in nautilus

--- modules/applications/file-managers/nautilus/default.nix
+++ modules/applications/file-managers/nautilus/default.nix
@@ -1,7 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   os.services.gvfs.enable = true;
   os.environment.systemPackages = [ pkgs.nautilus ];
-
+  os.environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+    lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+      (
+        with pkgs.gst_all_1;
+        [
+          gst-plugins-good
+          gst-plugins-bad
+          gst-plugins-ugly
+          gst-libav
+        ]
+      );
   settings.windows.no-bar = [ "org.gnome.Nautilus" ];
 }
