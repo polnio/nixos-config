b0ac71e79ad65fe9535a0c79047f05ea966ae1d2
Po Co <polniolino@gmail.com>
2024-08-01 01:10:14 +0200

    Fix minecraft bedrock keyboard issue

--- modules/applications/games/lutris/default.nix
+++ modules/applications/games/lutris/default.nix
@@ -7,10 +7,13 @@
         pkgs.e2fsprogs
         pkgs.libgpg-error
         pkgs.xorg.libICE
+        # pkgs.qt5.qtwayland
       ];
     })
   ];
   hm.xdg.dataFile."qt-plugins".source = "${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins/platforms";
+
+  os.environment.variables.QT_XKB_CONFIG_ROOT = [ "${pkgs.xkeyboard_config}/share/X11/xkb" ];
 
   settings.windows.no-bar = [ "lutris" ];
 }
