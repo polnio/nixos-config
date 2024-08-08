cb783d6e2f6904430ce1d055833a70c85d18fd8d
Po Co <polniolino@gmail.com>
2024-08-08 12:29:07 +0200

    Add catppuccin discord theme

--- modules/applications/social/discord/default.nix
+++ modules/applications/social/discord/default.nix
@@ -1,6 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
+  inputs.catppuccin-discord = {
+    url = "https://catppuccin.github.io/discord/dist/catppuccin-mocha-blue.theme.css";
+    flake = false;
+  };
+
   os.environment.systemPackages = [ pkgs.vesktop ];
+
+  hm.xdg.configFile."vesktop/themes/catppuccin.theme.css".source = inputs.catppuccin-discord;
+
   settings = {
     keymaps = [
       {
