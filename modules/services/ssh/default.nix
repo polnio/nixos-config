b3602c1adeef621577072910bdda6741ea4240c3
Po Co <polniolino@gmail.com>
2024-08-12 19:07:13 +0200

    Remove broken gitlab ssh config

--- modules/services/ssh/default.nix
+++ modules/services/ssh/default.nix
@@ -1,18 +1,4 @@
 { ... }:
 {
-  hm.programs.ssh = {
-    enable = true;
-    matchBlocks = {
-      "gitlab.com" = {
-        hostname = "altssh.gitlab.com";
-        user = "git";
-        port = 443;
-        extraOptions = {
-          PreferredAuthentications = "publickey";
-        };
-        identityFile = "~/.ssh/gitlab";
-      };
-    };
-  };
-
+  hm.programs.ssh.enable = true;
 }
