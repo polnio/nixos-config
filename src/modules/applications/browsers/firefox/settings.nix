3427078694697a7ec8abd0448266b6ab0f4238a0
Po Co <polniolino@gmail.com>
2025-03-27 14:39:26 +0100

    Set zen browser as default

--- src/modules/applications/browsers/firefox/settings.nix
+++ src/modules/applications/browsers/firefox/settings.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   settings = {
-    commands.browser = "${pkgs.firefox}/bin/firefox";
+    # commands.browser = "${pkgs.firefox}/bin/firefox";
     windows.no-bar = [ "firefox" ];
   };
 }
--- src/modules/applications/browsers/zen/settings.nix
+++ src/modules/applications/browsers/zen/settings.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 {
   settings = {
+    commands.browser = "${pkgs.zen-browser}/bin/zen";
     windows.no-bar = [ "zen" ];
   };
 }
