26f1b961868d51dbc9fddfec92dbfdc85e1c6b47
Po Co <polniolino@gmail.com>
2025-04-04 19:31:16 +0200

    nixvim: port telescope and oil keymaps to mini

--- src/modules/applications/editors/nixvim/config/plugins/mini.nix
+++ src/modules/applications/editors/nixvim/config/plugins/mini.nix
@@ -25,4 +25,47 @@
     };
     luaConfig.post = "vim.ui.select = require('mini.pick').ui_select";
   };
+  keymaps =
+    let
+      mkKeymap = keymap: {
+        mode = "n";
+        key = keymap.key;
+        action = "<cmd>${keymap.action}<cr>";
+        options.silent = true;
+      };
+    in
+    [
+      (mkKeymap {
+        key = "<leader>ff";
+        action = "Pick files";
+      })
+      (mkKeymap {
+        key = "<leader>fo";
+        action = "Pick oldfiles";
+      })
+      (mkKeymap {
+        key = "<leader>fb";
+        action = "Pick buffers";
+      })
+      (mkKeymap {
+        key = "<leader>fw";
+        action = "Pick grep_live";
+      })
+      (mkKeymap {
+        key = "<leader>fd";
+        action = "Pick diagnostics";
+      })
+      (mkKeymap {
+        key = "z=";
+        action = "Pick spellsuggest";
+      })
+      (mkKeymap {
+        key = "gs";
+        action = ''Pick lsp scope="workspace_symbol"'';
+      })
+      (mkKeymap {
+        key = "-";
+        action = "lua MiniFiles.open()";
+      })
+    ];
 }
