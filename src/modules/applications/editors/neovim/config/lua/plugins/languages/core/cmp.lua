fc8f62d86929279cd0ee4a8c44d3171cab3be0e0
Po Co <polniolino@gmail.com>
2025-04-16 09:50:16 +0100

    neovim: Use cmp for supermaven

--- src/modules/applications/editors/neovim/config/lua/plugins/languages/core/cmp.lua
+++ src/modules/applications/editors/neovim/config/lua/plugins/languages/core/cmp.lua
@@ -56,6 +56,7 @@ return {
         }),
       },
       sources = cmp.config.sources({
+        { name = "supermaven" },
         { name = "otter" },
         { name = "nvim_lsp" },
         -- { name = 'vsnip' }, -- For vsnip users.
--- src/modules/applications/editors/neovim/config/lua/plugins/languages/core/supermaven.lua
+++ src/modules/applications/editors/neovim/config/lua/plugins/languages/core/supermaven.lua
@@ -2,6 +2,7 @@ return {
   "supermaven-inc/supermaven-nvim",
   event = "BufEnter",
   opts = {
+    disable_inline_completion = true,
     keymaps = {
       accept_suggestion = "<C-z>",
       clear_suggestion = "<C-]>",
